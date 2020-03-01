e2c30f82d920bddaa6fa8d1fa1019ca1884ec5f4
brielle <briellemarie28@gmail.com>
2020-03-01 14:48:58 -0500

    more files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
+
+
+
 export class AppComponent {
   title = 'Scheduler';
+  public minDate: Date = new Date ("02/01/2020");
+  public maxDate: Date = new Date ("12/31/2020");
+  public value: Date = new Date ("02/29/2020");
 }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CalendarModule } from '@syncfusion/ej2-angular-calendars';
+import { FormsModule } from '@angular/forms';
+import {ScheduleModule, AgendaService, DayService,
+   WeekService, WorkWeekService, MonthService } from '@syncfusion/ej2-angular-schedule';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { TomorrowComponent } from './schedule/tomorrow/tomorrow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    TomorrowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ScheduleModule,
+    CalendarModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AgendaService, DayService, WeekService, WorkWeekService, MonthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
